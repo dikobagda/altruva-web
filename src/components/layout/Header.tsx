d5f17df1a05427a6fff778ec1f93ae4f6a51d6e1
Diko Bagda Anggara <dikobagda@gmail.com>
2025-07-23 15:03:57 +0000

    detach navitem "369 Harmony" from about us to after new menu after about

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -35,10 +35,10 @@ const navItems: NavItem[] = [
       { href: '/about-us', label: 'About us' },
       { href: '/about-us/meet-dr-olivia-aldisa', label: 'Meet dr. Olivia Aldisa' },
       { href: '/our-clinic', label: 'Our Clinic' },
-      { href: '/about-us/369-harmony', label: '369 Harmony™' },
       { href: '/news-media', label: 'News & Media' },
     ],
   },
+  { href: '/about-us/369-harmony', label: '369 Harmony™' },
   {
     label: 'Facial',
     subItems: [
