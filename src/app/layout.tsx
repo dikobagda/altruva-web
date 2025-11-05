e547940d85ea0d82a518d50d8c64913548313f9a
Diko Bagda Anggara <dikobagda@gmail.com>
2025-11-05 07:33:22 +0000

    apply google tag manager to : altruva.co.id/  Not tagged
    altruva.co.i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-M5XNZWDJ');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body 
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
@@ -42,6 +53,11 @@ export default function RootLayout({
           nunitoSans.variable
         )}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5XNZWDJ"
+        height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <NextTopLoader
           color="hsl(var(--primary))"
           initialPosition={0.08}
