e7e04c45324e18ebd4ab3c14c2069deb5c0534cb
Diko Bagda Anggara <dikobagda@gmail.com>
2025-11-07 23:10:04 +0000

    in section Aging is natural, update image using new asset /images/aging/

--- src/app/flyer/page.tsx
+++ src/app/flyer/page.tsx
@@ -90,22 +90,22 @@ export default function FlyerPage() {
     {
       number: '01',
       text: 'Kulit kendur di area pipi, rahang, & leher',
-      image: { src: 'https://picsum.photos/seed/concern1/400/400', hint: 'sagging skin' }
+      image: { src: '/images/aging/aging1.webp', hint: 'sagging skin' }
     },
     {
       number: '02',
       text: 'Garis halus & kerutan di sekitar mata dan dahi',
-      image: { src: 'https://picsum.photos/seed/concern2/400/400', hint: 'wrinkles forehead' }
+      image: { src: '/images/aging/aging2.webp', hint: 'wrinkles forehead' }
     },
     {
       number: '03',
       text: 'Kulit kusam efek penuaan dini',
-      image: { src: 'https://picsum.photos/seed/concern3/400/400', hint: 'dull skin' }
+      image: { src: '/images/aging/aging3.webp', hint: 'dull skin' }
     },
     {
       number: '04',
       text: 'Kontur wajah menurun',
-      image: { src: 'https://picsum.photos/seed/concern4/400/400', hint: 'drooping face' }
+      image: { src: '/images/aging/aging4.webp', hint: 'drooping face' }
     }
   ];
 
