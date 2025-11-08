3fbcd60de329456ce708ae5f2d74bd283434be8c
Diko Bagda Anggara <dikobagda@gmail.com>
2025-11-08 03:59:24 +0000

    add more 200px in the section

--- src/app/flyer/page.tsx
+++ src/app/flyer/page.tsx
@@ -215,7 +215,7 @@ export default function FlyerPage() {
       </SectionWrapper>
 
       {/* Jakarta's 1st Regenerative Clinic Section */}
-      <section className="relative w-full aspect-[16/9] md:aspect-[2.5/1]">
+      <section className="relative w-full" style={{ minHeight: '600px' }}>
         <Image
           src="/images/flyer/landingpage_banner_desktop.webp"
           alt="Jakarta's 1st Regenerative Contouring Clinic"
