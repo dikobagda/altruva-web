0e62904e1587d9553dd659d62efda4855ff7039d
dikobagda <dikobagda@gmail.com>
2026-08-02 01:02:46 +0700

    fix: update robots.ts to allow crawling of Next.js static assets and image optimizer

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,11 +6,21 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://altruva.co.id';
 
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/_next/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: [
+          '/',
+          '/_next/static/',
+          '/_next/image',
+        ],
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/cms/',
+        ],
+      }
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
